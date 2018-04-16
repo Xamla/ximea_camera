54d4e6f0037135e9ca98a3d2ae04cd2582e5f2d3
ialtrogge <inga.altrogge@xamla.com>
2018-04-16 14:35:30 +0200

    Update trigger_mono.lua

--- ros/examples/trigger_mono.lua
+++ ros/examples/trigger_mono.lua
@@ -1,58 +1,68 @@
 local ros = require 'ros'
 require 'ximea.ros.XimeaClient'
-cv = require 'cv'
-require 'cv.highgui'
+local cv = require 'cv'
 require 'cv.imgproc'
 require 'cv.imgcodecs'
-
-
-local CAMERA_SERIALS = {
-  board ='40600628',   -- pcb board pose measuring and visual servoing cam
-  scan = '28670151',   -- structured light scanning camera
-  onboard = '35371951', -- mounted on robot
-  spare = '28674051',
-}
+require 'cv.highgui'
 
 
 local function main()
+
+  -- initialize ROS
   ros.init('trigger_camera')
+  local nh = ros.NodeHandle()
+  local sp = ros.AsyncSpinner()
+  sp:start()
 
   local cmd = torch.CmdLine()
   cmd:text()
   cmd:text('Set exposure utility')
   cmd:text()
   cmd:option('-cam', 'scan', 'camera name one of [board, scan, onboard], empty for all')
-  cmd:option('-serial', '', 'serial number')
-  cmd:option('-time', 20, 'time in ms')
-  cmd:option('-frames', 24, 'number of frames')
+  cmd:option('-serial', '28674051', 'serial number')
+  cmd:option('-exposure', 20000, 'exposure time in micro seconds')
+  cmd:option('-frames', 12, 'number of frames')
+  cmd:option('-time', 20, 'time in milliseconds')
+  cmd:option('-action_name', '/ximea_mono/trigger', 'ximea action name')
   local opt = cmd:parse(arg or {})
 
-  local xc = XimeaClient(nh, 'ximea_mono', false, false)
+  --local ximea_client = XimeaClient(nh, 'ximea_mono', false, false)
+  local ximea_client = XimeaClient(nh, 'ximea_mono', false, false, nil, "/ximea_mono/trigger")
+  local camera_serial = opt.serial
+  local exposure = opt.exposure
+  ximea_client:setExposure(exposure, {camera_serial})
 
-  local serial
-  if opt.cam ~= '' then
-    serial = CAMERA_SERIALS[opt.cam]
-  elseif opt.serial ~= '' then
-    serial = opt.serial
+  -- triggering test
+  local t = torch.Timer()
+  local images
+  local output
+  local frames = opt.frames
+  local time = opt.time -- time in ms
+  local ok = pcall(function() output = ximea_client:trigger(camera_serial, frames, time * 1000) end)
+  if ok then
+    print('Triggering worked fine with output:')
+    print(output)
+    images = output
+  else
+    print('Triggering failed with error:')
+    print(output)
   end
+  print("Got " .. #images .. " images in " .. t:time().real .. " seconds.")
 
-  if serial and opt.frames then
-    local t = torch.Timer()
-    local images
-    local ok, output = pcall(function() xc:trigger(serial, opt.frames, opt.time * 1000) end)
-    if ok then
-      images = output
-    else
-      error('Triggering failed. Error: ' .. output)
+  -- write images to disk
+  local outputDirectoryId = os.date("%Y-%m-%d_%H%M%S")
+  local outputDirectory = path.join("images/", outputDirectoryId)
+  os.execute("mkdir -p " .. outputDirectory)
+  for i = 1, #images do
+    if images[i]:nDimension() > 2 then
+      images[i] = cv.cvtColor{images[i], nil, cv.COLOR_RGB2BGR}
     end
-    print("Got " .. #images .. " images in " .. t:time().real)
-    for i = 1, #images do
-      cv.imwrite{"/tmp/" .. i .. ".jpg", images[i]}
-    end
+    cv.imwrite{outputDirectory .. "/" .. i .. ".jpg", images[i]}
   end
+  print(string.format("Wrote images to folder %s.", outputDirectory))
 
-  xc:shutdown()
-
+  ximea_client:shutdown()
+  sp:stop()
   ros.shutdown()
 end
 
