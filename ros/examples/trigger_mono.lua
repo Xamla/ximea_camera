090a13395a4daa60e3f9ece5802ccb5fe5f28a25
ialtrogge <inga.altrogge@xamla.com>
2018-04-16 14:56:17 +0200

    Update trigger_mono.lua

--- ros/examples/trigger_mono.lua
+++ ros/examples/trigger_mono.lua
@@ -37,28 +37,27 @@ local function main()
   local output
   local frames = opt.frames
   local time = opt.time -- time in ms
-  local ok = pcall(function() output = ximea_client:trigger(camera_serial, frames, time * 1000) end)
+  local ok, err = pcall(function() output = ximea_client:trigger(camera_serial, frames, time * 1000) end)
   if ok then
     print('Triggering worked fine with output:')
     print(output)
     images = output
+    print("Got " .. #images .. " images in " .. t:time().real .. " seconds.")
+    -- write images to disk
+    local outputDirectoryId = os.date("%Y-%m-%d_%H%M%S")
+    local outputDirectory = path.join("images/", outputDirectoryId)
+    os.execute("mkdir -p " .. outputDirectory)
+    for i = 1, #images do
+      if images[i]:nDimension() > 2 then
+        images[i] = cv.cvtColor{images[i], nil, cv.COLOR_RGB2BGR}
+      end
+      cv.imwrite{outputDirectory .. "/" .. i .. ".jpg", images[i]}
+    end
+    print(string.format("Wrote images to folder %s.", outputDirectory))
   else
     print('Triggering failed with error:')
-    print(output)
+    print(err)
   end
-  print("Got " .. #images .. " images in " .. t:time().real .. " seconds.")
-
-  -- write images to disk
-  local outputDirectoryId = os.date("%Y-%m-%d_%H%M%S")
-  local outputDirectory = path.join("images/", outputDirectoryId)
-  os.execute("mkdir -p " .. outputDirectory)
-  for i = 1, #images do
-    if images[i]:nDimension() > 2 then
-      images[i] = cv.cvtColor{images[i], nil, cv.COLOR_RGB2BGR}
-    end
-    cv.imwrite{outputDirectory .. "/" .. i .. ".jpg", images[i]}
-  end
-  print(string.format("Wrote images to folder %s.", outputDirectory))
 
   ximea_client:shutdown()
   sp:stop()
