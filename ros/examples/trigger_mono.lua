6b399e4eab36fe846ea73f82a1c2236b4e54ac18
Jan Nörtemann <jan.noertemann@xamla.com>
2018-05-15 17:06:32 +0200

    Add rate_limit for multiple cameras and further improvements
    - sanity checks when a new goal is received
    - send feedback at 5hz during trigger
    - remove start/stop acquisition calls during (de)activation of trigger,
      so listeners can stay connected to the images publishers

--- ros/examples/trigger_mono.lua
+++ ros/examples/trigger_mono.lua
@@ -27,41 +27,46 @@ local function main()
   local opt = cmd:parse(arg or {})
 
   local ximea_client = XimeaClient(nh, 'ximea_mono', false, false, nil, opt.action_name)
-  local serials = {'CAMAU1639042', 'CAMAU1710001'}
+  local serials = {'CAMAU1710001', 'CAMAU1639042'}
   ximea_client:setExposure(opt.exposure, serials)
 
   -- triggering test
-  local t = torch.Timer()
-  local images
+  local image_set
   local output
 
-  local exposures = {10000, 10000}
-  local ok, err = pcall(function() output = ximea_client:trigger(serials, 2, exposures, 1000) end)
-  if ok then
-    print('Triggering worked fine with output:')
-    print(output)
-    images = output
-    print("Got " .. #images .. " images in " .. t:time().real .. " seconds.")
-    -- write images to disk
-    local outputDirectoryId = os.date("%Y-%m-%d_%H%M%S")
-    local outputDirectory = path.join("images/", outputDirectoryId)
-    os.execute("mkdir -p " .. outputDirectory)
-    for i = 1, #images do
-      if images[i]:nDimension() > 2 then
-        images[i] = cv.cvtColor{images[i], nil, cv.COLOR_RGB2BGR}
+  local exposures = {20000, 20000}
+  --for i = 1, 100 do
+    local t = torch.Timer()
+    local ok, err = pcall(function() output = ximea_client:trigger(serials, 24, exposures, 0) end)
+    if ok then
+      print('Triggering worked fine')
+      image_set = output
+      print("Got " .. #image_set .. " image sets in " .. t:time().real .. " seconds.")
+      -- write images to disk
+      local outputDirectoryId = os.date("%Y-%m-%d_%H%M%S")
+      local outputDirectory = path.join("/home/xamla/code/tmp/", outputDirectoryId)
+      os.execute("mkdir -p " .. outputDirectory)
+      for n = 1, #image_set do
+        local images = image_set[n]
+        for i = 1, #images do
+          if images[i]:nDimension() > 2 then
+            images[i] = cv.cvtColor{images[i], nil, cv.COLOR_RGB2BGR}
+          end
+          cv.imwrite{outputDirectory .. "/" .. n .. "_" .. i .. ".jpg", images[i]}
+        end
       end
-      cv.imwrite{outputDirectory .. "/" .. i .. ".jpg", images[i]}
+      print(string.format("Wrote images to folder %s.", outputDirectory))
+    else
+      print('Triggering failed with error:')
+      print(err)
     end
-    print(string.format("Wrote images to folder %s.", outputDirectory))
-  else
-    print('Triggering failed with error:')
-    print(err)
-  end
-
+    print('end of loop #', i)
+  --end
   ximea_client:shutdown()
   sp:stop()
   ros.shutdown()
 end
 
 
-main()
+
+  main()
--- ros/ximea_nodes/mono.lua
+++ ros/ximea_nodes/mono.lua
@@ -19,7 +19,7 @@ local action_server
 local cameras = {}
 local opt   -- command line options
 local goal_state
-
+local image_array_spec = ros.MsgSpec('ximea_msgs/ImageArray')
 
 local function keys(t)
   local l = {}
@@ -100,11 +100,21 @@ local function openCameras()
   local serials = configuredSerialNumbers
   if serials == nil or #serials == 0 then
     serials = ximea.getSerialNumbers()     -- all cameras
+    configuredSerialNumbers = serials
+  end
+
+  local rate_limit
+  if (#serials > 1) then
+    --ros.INFO('[mono] Detected several cameras -> Disabling auto bandwidth calculation')
+    local XI_OFF = 0
+    --local status = ximea.m3api.xiSetParamInt(0, ximea.XI_PRM_AUTO_BANDWIDTH_CALCULATION, XI_OFF)
+    --ros.INFO('[mono] set auto band width: %d (%s)', status, ximea.XI_RET_TEXT[status])
+    rate_limit = (2400 / #serials) * 0.9
   end
 
   for i,serial in ipairs(serials) do
     local mode = configuredModes[i] or opt.mode
-    local cam = xr.XimeaRosCam(nh, serial, mode, {})
+    local cam = xr.XimeaRosCam(nh, serial, mode, {rateLimit = rate_limit})
     cameras[serial] = cam
   end
 end
@@ -247,11 +257,22 @@ end
   return true
 end--]]
 
-local function sendFeedback(targetFrameCount, currentFrameCount, errorMessage)
+local function sendFeedback(currentFrameCount, errorMessage)
   local fb = goal_state.handle:createFeeback()
-  fb.status = torch.Tensor({0, 0})
+  local status = {}
+  fb.serials = {}
+  for key, serial in ipairs(goal_state.camera_serials) do
+    local camera = cameras[serial]
+    if camera ~= nil then
+      table.insert(status, camera:getState().mode)
+    end
+    table.insert(fb.serials, serial)
+  end
+  
+  fb.status = torch.IntTensor(status)
   fb.serials = goal_state.camera_serials
-  fb.total_frame_count = targetFrameCount or 0
+  fb.total_frame_count = goal_state.target_frame_count or 0
+  fb.total_frame_count = fb.total_frame_count * #fb.serials
   fb.number_of_taken_frames = currentFrameCount or 0
   fb.error_message = errorMessage or ""
   goal_state.handle:publishFeedback(fb)
@@ -269,6 +290,21 @@ local function handleNewTriggerGoal(as)
   end
 
   print('Goal serials: ', goal.goal.serials)
+
+  if (#goal.goal.serials == 0 or goal.goal.exposure_times_in_microseconds:size(1) == 0) then
+    ros.WARN("Received goal without serials or exposire times. Aborting.", goal.goal.serial)
+    local r = as:createResult()
+    as:setAborted(r, 'Received goal without serials or exposire times.')
+    return
+  end
+
+  if (#goal.goal.serials ~= goal.goal.exposure_times_in_microseconds:size(1)) then
+    ros.WARN("Received goal with mismatching length of serials and exposure times. Aborting.", goal.goal.serial)
+    local r = as:createResult()
+    as:setAborted(r, 'Missmatch of array length of serials and exposure_times_in_microseconds')
+    return
+  end
+
   for key, serial in ipairs(goal.goal.serials) do
     if not hasValue(configuredSerialNumbers, serial) then
       ros.WARN("Received goal for unkown serial number %s. Aborting.", goal.goal.serial)
@@ -285,32 +321,50 @@ local function handleNewTriggerGoal(as)
   goal_state.time_out_in_ms = goal.goal.timeout_in_ms
   goal_state.status = 0
   goal_state.handle = action_server:getCurrentGoalHandle()
+  goal_state.last_frame_check = ros.Time.now()
+  goal_state.last_feedback_publish = ros.Time.now()
+  goal_state.frame_check_interval = goal.goal.exposure_times_in_microseconds:min() 
+  goal_state.target_frame_count = goal.goal.frame_count
   sendFeedback()
+
   for key, serial in ipairs(goal.goal.serials) do
     local camera = cameras[serial]
     if camera ~= nil then
       camera:startTrigger(goal.goal.frame_count, goal.goal.exposure_times_in_microseconds[key])
     else
-      ros.WARN('Camera %s not found', goal.goal.serial)
+      ros.WARN('Camera %s not found', serial)
     end
   end
+
   goal_state.status = 1
+  sendFeedback()
 end
 
 
 local function checkAllCamerasInGoalForNewFrames()
-  local result_state = {}
+  local frame_count = 0
+  local time_since_last_check = ros.Time.now() - goal_state.last_frame_check 
   for key, serial in ipairs(goal_state.camera_serials) do
     local camera = cameras[serial]
+    local state = camera:getState()
     if camera ~= nil then
-      local state = camera:getState()
       if (state.mode == 1) then -- trigger mode
-        camera:checkForNewFrame()
+        if (#state.frames < goal_state.target_frame_count) then
+          if (time_since_last_check:toSec() * 1000000 > goal_state.frame_check_interval) then
+            goal_state.last_frame_check = ros.Time.now()
+            camera:checkForNewFrame()
+            state = camera:getState()
+          end
+        end
       else
         ros.ERROR('Camera %s in wrong mode', serial)
       end
+
+      frame_count = frame_count + #state.frames
     end
   end
+
+  return frame_count
 end
 
 
@@ -357,7 +411,7 @@ end
 local function handleTriggerCompleted()
   ros.INFO('Completed trigger goal')
   goal_state.status = 2
-  sendFeedback(state.target_frame_count, #state.frames)
+  
 
   local r = action_server:createResult()
   r.serials = goal_state.camera_serials
@@ -366,7 +420,6 @@ local function handleTriggerCompleted()
     local camera = cameras[serial]
     if camera ~= nil then
       local state = camera:getState()
-      local image_array_spec = ros.MsgSpec('ximea_msgs/ImageArray')
       local msg = ros.Message(image_array_spec)
       msg.header.stamp = ros.Time.now()
       msg.header.frame = serial
@@ -375,13 +428,20 @@ local function handleTriggerCompleted()
       msg.images = state.frames
       total_frames = total_frames + #state.frames
       table.insert(r.images_all_cameras, msg);
-      camera:stopTrigger()
     end
   end
 
+  sendFeedback(total_frames)
   r.total_frame_count = total_frames
-
   action_server:setSucceeded(r, 'Captured ' .. total_frames .. 'frames')
+  ros.INFO('Goal succeeded')
+
+  for key, serial in ipairs(goal_state.camera_serials) do
+    local camera = cameras[serial]
+    if camera ~= nil then
+      camera:stopTrigger()
+    end
+  end
 
   goal_state = nil
 end
@@ -390,11 +450,15 @@ end
 local function triggerWorker()
   if goal_state ~= nil then
     local time = ros.Time.now() - goal_state.start_time
-    print('trigger timeout', goal_state.time_out_in_ms, time:toSec() * 1000 > goal_state.time_out_in_ms)
     if goal_state.time_out_in_ms > 0 and time:toSec() * 1000 > goal_state.time_out_in_ms then
       handleTriggerTimeout()
     else
-      checkAllCamerasInGoalForNewFrames()
+      local frame_count = checkAllCamerasInGoalForNewFrames()
+      local time_since_last_feedback = ros.Time.now() - goal_state.last_feedback_publish
+      if (time_since_last_feedback:toSec() > 0.2) then
+        sendFeedback(frame_count)
+        goal_state.last_feedback_publish = ros.Time.now()
+      end
       if isTriggerCompleted() then
         handleTriggerCompleted()
       end
